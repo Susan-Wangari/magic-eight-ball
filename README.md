7353d87c58faecf11c12b950a0ce4da42dcb4580
Susan-Wangari <susanwangari810@gmail.com>
2021-04-26 12:49:30 -0700

    magic 8 ball in JAvaScript

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # magic-eight-ball
+It generates a random number between 0 and 7 and gives responses to each random number
---
+++ main.js
@@ -0,0 +1,38 @@
+let userName = " ";
+const userQuestion = " When is the right time to get married?";
+//generates a randon number between 0 and 7
+let randomNumber = Math.floor(Math.random() * 8);
+let eightBall = " ";
+//using string interpolation and the ternary operator
+ (userName === 'Jane') ?  console.log(`Hello, ${userName}!`) :  console.log('Hello!');
+//logging the user question
+ console.log(`The user asked, ${userQuestion}`);
+
+ switch(randomNumber) {
+     case 0:
+     eightBall = 'It is certain';
+     break;
+     case 1: 
+     eightBall = 'It is decidedly so';
+     break;
+     case 2: 
+     eightBall = 'Reply hazy try again';
+     break;
+     case 3:
+     eightBall = 'Cannot predict now';
+     break;
+     case 4:
+     eightBall = 'Do not count on it';
+     break;
+     case 5:
+     eightBall = 'My sources say no';
+     break;
+     case 6:
+     eightBall = 'Outlook not so good';
+     break;
+     case 7:
+     eightBall = 'Signs point to yes';
+     break;
+ }
+ 
+ console.log(eightBall);
